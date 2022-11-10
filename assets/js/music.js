5e6c28247d3706e0f4950ae3861b6d30acd88629
Samuel Barubiriza <samuel.barubiriza@setiae.com>
2022-11-10 23:49:40 +0100

    adding js music and new text for playlist

--- assets/js/music.js
+++ assets/js/music.js
@@ -2,6 +2,8 @@ let titleFisrtSection = document.querySelector('.block-title span.title1');
 let titleSecondSection = document.querySelector('.block-title span.title2');
 let titleThirdSection = document.querySelector('.block-title span.title3');
 let titleDotsSection = document.querySelector('.block-title span.title-dots');
+let epCover = document.querySelector('.album-infos-container img.ep-js');
+let epIframe = document.querySelector('.iframe-js');
 
 window.onload = function(){
    
@@ -12,5 +14,21 @@ window.onload = function(){
     titleThirdSection.style.opacity = '1';
     
     titleDotsSection.style.opacity = '1';
-    
+
 }
+epCover.addEventListener('mouseenter',() => {
+        
+    epIframe.classList.add('iframe-js-show');
+    epIframe.addEventListener('mouseenter', ()=> {
+        epIframe.classList.add('iframe-js-show');
+    });
+    epIframe.addEventListener('mouseleave',() => {
+
+        epIframe.classList.remove('iframe-js-show');
+    });
+    
+});
+epCover.addEventListener('mouseleave',() => {
+
+    epIframe.classList.remove('iframe-js-show');
+});
